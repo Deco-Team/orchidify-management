dae7dab84aa90967dd9f3077f698cedc97449502
sarahnguyen <thanhhao2832@gmail.com>
2024-09-05 01:14:05 +0700

    [UI][HaoNTT] Add sidebar component

---
+++ src/components/sidebar/Option.ts
@@ -0,0 +1,18 @@
+export const OptionsAdmin = [
+  { id: 1, text: 'Trang chủ', link: 'dashboard' },
+  { id: 2, text: 'Khóa học', link: 'courses' }
+]
+
+export const OptionsStaff = [
+  { id: 1, text: 'Trang chủ', link: 'dashboard' },
+  { id: 2, text: 'Khóa học', link: 'courses' },
+  { id: 3, text: 'Yêu cầu khóa học', link: 'courseReqs' },
+  { id: 4, text: 'Đơn tuyển', link: 'recruiments' },
+  { id: 5, text: 'Giảng viên', link: 'instructors' },
+  { id: 6, text: 'Học viên', link: 'learners' },
+  { id: 7, text: 'Nhà vườn', link: 'garden' },
+  { id: 8, text: 'Quản lý vườn', link: 'gardenMng' },
+  { id: 9, text: 'Yêu cầu rút tiền', link: 'withdrawReqs' }
+]
+
+export const OptionsGardenMng = [{ id: 1, text: 'Quản lý vườn', link: 'gardenMng' }]
